4d6d652e16124d94ca268fc59720f55f89b57065
oussama <oussamajallali6@gmail.com>
2022-02-03 21:11:01 +0100

    Add poc features

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Assets/css/App.css";
+import Map from "./Pages/Map/map";
+
+function App() {
+  return <Map />;
+}
+
+export default App;
---
+++ src/Components/index.ts
@@ -0,0 +1,2 @@
+export { default as ArtMap } from "./Artwork.map";
+export { default as Pin } from "./Pin.map";
