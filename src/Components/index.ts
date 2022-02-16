e5e9f51faa028fea14d7ffc34571dc01a615f316
oussama <oussamajallali6@gmail.com>
2022-02-16 12:59:02 +0100

    Add form validation, connection backend and update route

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,5 @@
 export { default as ArtMap } from "./Artwork.mapAdmin";
 export { default as ArtworkUser } from "./Artwork.mapUser";
 export { default as Pin } from "./Pin.map";
-export { default as CreateArtWork } from "./Artwork.formAdmin";
+// export { default as CreateArtWork } from "./Artwork.formAdmin";
+export { default as CreateArtWork } from "./AdminCreateArtwork";
