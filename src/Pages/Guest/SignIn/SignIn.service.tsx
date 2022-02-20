84c71871a5781fa20eea2df0a254c04e389e5ce3
oussama <oussamajallali6@gmail.com>
2022-02-20 16:43:36 +0100

    Update paths2

--- src/Pages/Guest/SignIn/SignIn.service.tsx
+++ src/Pages/Guest/SignIn/SignIn.service.tsx
@@ -10,16 +10,19 @@ export const login = async (
 ) => {
   setIsLoading(true);
   try {
-    const data: Response = await fetch("http://localhost:3008/auth/login", {
-      method: "POST",
-      body: JSON.stringify({
-        email: creds.email,
-        password: creds.password,
-      }),
-      headers: {
-        "Content-type": "Application/json",
-      },
-    });
+    const data: Response = await fetch(
+      "http://srv-dpi-proj-openstreetart22-test.univ-rouen.fr/api/auth/login",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: creds.email,
+          password: creds.password,
+        }),
+        headers: {
+          "Content-type": "Application/json",
+        },
+      }
+    );
     if (data.ok) {
       const jsonData = await data.json();
       localStorage.setItem("jwt", jsonData.jwt);
--- src/Pages/Guest/SignUp/SignUp.services.ts
+++ src/Pages/Guest/SignUp/SignUp.services.ts
@@ -1,15 +1,18 @@
 import User from "./types/user";
 
 const register = async (user: User) => {
-  const data: Response = await fetch("http://localhost:3008/auth/register", {
-    method: "POST",
-    body: JSON.stringify({
-      ...user,
-    }),
-    headers: {
-      "Content-type": "Application/json",
-    },
-  });
+  const data: Response = await fetch(
+    "http://srv-dpi-proj-openstreetart22-test.univ-rouen.fr/api/auth/register",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        ...user,
+      }),
+      headers: {
+        "Content-type": "Application/json",
+      },
+    }
+  );
   return data;
 };
 
