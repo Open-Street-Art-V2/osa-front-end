2e72a7f44454abe1f3078eccf73efcf1228267de
oussama <oussamajallali6@gmail.com>
2022-03-06 06:43:16 +0100

    Add validateProp service and reorganize code

---
+++ src/Pages/Admin/ValidateProp/ValidateProp.service.ts
@@ -0,0 +1,101 @@
+/* eslint-disable no-unused-vars */
+export const getProposals = async (
+  isContributions: boolean,
+  currentPage: number,
+  token: string | undefined,
+  setHasMoreProp: (hasMoreProp: boolean) => void,
+  setAllArtwork: (allArtwork: any) => void,
+  setCheckedProposals: (checkedProposals: any) => void,
+  setCurrentPage: (currentPage2: number) => void,
+  setIsLoading: (isLoading: boolean) => void
+) => {
+  let url = "";
+  if (!isContributions) {
+    url = `${process.env.REACT_APP_GET_PROPOSALS}?page=${currentPage}&limit=10`;
+  } else {
+    url = `${process.env.REACT_APP_GET_CONTRIBUTION}?page=${currentPage}&limit=10`;
+  }
+  const res: Response = await fetch(url, {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  const res2 = await res.json();
+  if (res.ok) {
+    const data = res2.items;
+    if (data.length < 1) {
+      setHasMoreProp(false);
+    }
+    setAllArtwork((old: any) => [...old, ...data]);
+    const proposals = new Array(data.length).fill(null).map(() => ({
+      checked: false,
+      id: 0,
+    }));
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < data.length; i++) {
+      proposals[i].id = data[i].id.toString();
+    }
+    setCheckedProposals((old: any) => [...old, ...proposals]);
+    const nextPage = currentPage + 1;
+    setCurrentPage(nextPage);
+    setIsLoading(false);
+  }
+};
+
+export const validateProposals = async (
+  isContributions: boolean,
+  token: string | undefined,
+  proposals: any,
+  setUpdateComp: (updateComp: boolean) => void
+) => {
+  let url = "";
+  if (!isContributions) {
+    url = `${process.env.REACT_APP_VALIDATE_PROPOSALS}`;
+  } else {
+    url = `${process.env.REACT_APP_VALIDATE_CONTRIBUTION}`;
+  }
+  const bodyPost = JSON.stringify({ propositions: proposals });
+  if (url) {
+    const res: Response = await fetch(url, {
+      method: "POST",
+      body: bodyPost,
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    });
+    if (res.ok) {
+      setUpdateComp(true);
+    }
+  }
+};
+
+export const rejectProposals = async (
+  isContributions: boolean,
+  token: string | undefined,
+  proposals: any,
+  setUpdateComp: (updateComp: boolean) => void
+) => {
+  let url = "";
+  if (!isContributions) {
+    url = `${process.env.REACT_APP_GET_PROPOSALS}`;
+  } else {
+    url = `${process.env.REACT_APP_GET_CONTRIBUTION}`;
+  }
+
+  const bodyPost = JSON.stringify({ propositions: proposals });
+  const res: Response = await fetch(url, {
+    method: "DELETE",
+    body: bodyPost,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  });
+  if (res.ok) {
+    setUpdateComp(true);
+  }
+  return res;
+};
--- src/Pages/Admin/ValidateProp/ValidateProp.tsx
+++ src/Pages/Admin/ValidateProp/ValidateProp.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useContext } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useNavigate, Link } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
-import { Alert } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { Header, ArtworkProposal } from "../../../Components";
 import { LoginContext } from "../../../Components/Context/LoginCtxProvider";
-import "./ValidateProp.css";
+import {
+  validateProposals,
+  rejectProposals,
+  getProposals,
+} from "./ValidateProp.service";
 
 function ValidateProp() {
   const [allArtwork, setAllArtwork] = useState<any[]>([]);
@@ -16,9 +17,10 @@ function ValidateProp() {
   const [hasMoreProp, setHasMoreProp] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
   const [isContributions, setIsContributions] = useState(false);
-
+  const [updateComp, setUpdateComp] = useState(false);
   const navigate = useNavigate();
   const loginCtx = useContext(LoginContext);
+  const skeletons = [1, 2, 3, 4];
 
   useEffect(() => {
     if (loginCtx.user?.role !== "ROLE_ADMIN") {
@@ -27,7 +29,6 @@ function ValidateProp() {
   }, [loginCtx]);
 
   const handleAllProposalsChange = () => {
-    // console.log(isAllChecked);
     const updatedCheckedState = checkedProposals.map((item: any) => ({
       checked: !isAllChecked,
       id: item.id,
@@ -36,7 +37,6 @@ function ValidateProp() {
     setIsAllChecked(!isAllChecked);
   };
 
-  // Change Proposal state (true : if checked, flase : if not)
   const handleProposalChange = (position: number) => {
     const updatedCheckedState = checkedProposals.map(
       (item: any, index: number) =>
@@ -45,211 +45,79 @@ function ValidateProp() {
     setCheckedProposals(updatedCheckedState);
   };
 
-  async function sendValidatedProposals(prop: any) {
-    let url = "";
-    if (!isContributions) {
-      url = "http://localhost:3008/proposition/validate";
-    } else {
-      url = "http://localhost:3008/contribution/validMany";
-    }
-    // const formData = new FormData();
-    const proposals = new Array(prop.length).fill(null);
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < prop.length; i++) {
-      // formData.append("propositions", prop[i].id);
-      proposals[i] = Number(prop[i].id);
-      // console.log(prop[i].id);
-    }
-    console.log(proposals);
-    const bodyPost = JSON.stringify({ propositions: proposals });
-    console.log(bodyPost);
-    if (url) {
-      const res: Response = await fetch(url, {
-        method: "POST",
-        body: bodyPost,
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${loginCtx.user?.jwt}`,
-        },
-      });
-      if (res.ok) {
-        console.log("done");
-        /* const newAllArtwork = allArtwork.filter((item: any) => item.id !== "8");
-        console.log(newAllArtwork);
-        setCheckedProposals([]);
-        React.useCallback(() => setAllArtwork(newAllArtwork), []); */
-        window.location.reload();
-      }
-    }
-  }
-
   const handleValidateProposals = () => {
     const prop = checkedProposals.filter((item: any) => item.checked);
-
-    const newArray = prop.map(function (item: any) {
-      // eslint-disable-next-line no-param-reassign
-      delete item.checked;
-      return item;
-    });
-    console.log(prop);
-    console.log(newArray);
-    sendValidatedProposals(newArray);
+    if (prop.length > 0) {
+      const newArray = prop.map((item: any) => {
+        // eslint-disable-next-line no-param-reassign
+        delete item.checked;
+        return item;
+      });
+      const proposals = new Array(newArray.length).fill(null);
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < newArray.length; i++) {
+        proposals[i] = Number(newArray[i].id);
+      }
+      validateProposals(
+        isContributions,
+        loginCtx.user?.jwt,
+        proposals,
+        setUpdateComp
+      );
+    }
   };
 
-  async function sendDeleteProposals(prop: any) {
-    let url = "";
-    if (!isContributions) {
-      url = "http://localhost:3008/proposition";
-    } else {
-      url = "http://localhost:3008/contribution";
-    }
-    // const formData = new FormData();
-    const proposals = new Array(prop.length).fill(null);
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < prop.length; i++) {
-      // formData.append("propositions", prop[i].id);
-      proposals[i] = Number(prop[i].id);
-      // console.log(prop[i].id);
-    }
-    console.log(proposals);
-    const bodyPost = JSON.stringify({ propositions: proposals });
-    console.log(bodyPost);
-    if (url) {
-      const res: Response = await fetch(url, {
-        method: "DELETE",
-        body: bodyPost,
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${loginCtx.user?.jwt}`,
-        },
-      });
-      if (res.ok) {
-        console.log("DELETE : done");
-        /* const newAllArtwork = allArtwork.filter((item: any) => item.id !== "8");
-        console.log(newAllArtwork);
-        setCheckedProposals([]);
-        React.useCallback(() => setAllArtwork(newAllArtwork), []); */
-        // window.location.reload();
-      }
-    }
-  }
-
-  const handleDeleteProposals = () => {
+  const handleRejectProposals = () => {
     const prop = checkedProposals.filter((item: any) => item.checked);
-
-    const newArray = prop.map(function (item: any) {
-      // eslint-disable-next-line no-param-reassign
-      delete item.checked;
-      return item;
-    });
-    sendDeleteProposals(newArray);
+    if (prop.length > 0) {
+      const newArray = prop.map((item: any) => {
+        // eslint-disable-next-line no-param-reassign
+        delete item.checked;
+        return item;
+      });
+      const proposals = new Array(newArray.length).fill(null);
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < newArray.length; i++) {
+        proposals[i] = Number(newArray[i].id);
+      }
+      rejectProposals(
+        isContributions,
+        loginCtx.user?.jwt,
+        proposals,
+        setUpdateComp
+      );
+    }
   };
-
-  async function fetchMoreData() {
-    const url = `${process.env.REACT_APP_GET_PROPOSALS}?page=${currentPage}&limit=10`;
-    if (url) {
-      const res: Response = await fetch(url, {
-        method: "GET",
-        headers: {
-          authorization: `Bearer ${loginCtx.user?.jwt}`,
-        },
-      });
-      const res2 = await res.json();
-      if (res.ok) {
-        const data = res2.items;
-        // console.log(data);
-        if (data.length < 1) {
-          setHasMoreProp(false);
-        }
-        setAllArtwork((old: any) => [...old, ...data]);
-        const proposals = new Array(data.length).fill(null).map(() => ({
-          checked: false,
-          id: 0,
-        }));
-
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < data.length; i++) {
-          proposals[i].id = data[i].id.toString();
-        }
-        setCheckedProposals((old: any) => [...old, ...proposals]);
-        const nextPage = currentPage + 1;
-        console.log(nextPage);
-        setCurrentPage(nextPage);
-        setIsLoading(false);
-      }
-    }
-  }
-  const skeletons = [1, 2, 3, 4];
-
-  useEffect(() => {
-    // setIsLoading(true);
-    // fetchMoreData();
-  }, []);
-
-  async function fetchMoreDataContrib() {
-    console.log("hi");
-    const url = `${process.env.REACT_APP_VALIDATE_CONTRIBUTION}?page=${currentPage}&limit=10`;
-    if (url) {
-      const res: Response = await fetch(url, {
-        method: "GET",
-        headers: {
-          authorization: `Bearer ${loginCtx.user?.jwt}`,
-        },
-      });
-      const res2 = await res.json();
-      if (res.ok) {
-        const data = res2.items;
-        // console.log(data);
-        if (data.length < 1) {
-          setHasMoreProp(false);
-        }
-        setAllArtwork((old: any) => [...old, ...data]);
-        const proposals = new Array(data.length).fill(null).map(() => ({
-          checked: false,
-          id: 0,
-        }));
-
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < data.length; i++) {
-          proposals[i].id = data[i].id.toString();
-        }
-        setCheckedProposals((old: any) => [...old, ...proposals]);
-        const nextPage = currentPage + 1;
-        console.log(nextPage);
-        setCurrentPage(nextPage);
-        setIsLoading(false);
-      }
-    }
-  }
 
   function handleSwitchChange() {
-    console.log("switch change");
-    setCheckedProposals([]);
-    setIsAllChecked(false);
-    setHasMoreProp(true);
-    setIsLoading(true);
-    setAllArtwork([]);
-    setCurrentPage(1);
-
-    /* if (isContributions) {
-      console.log("fetchMoreData");
-      console.log(currentPage);
-      fetchMoreData();
-    } else {
-      console.log("fetchMoreDataContrib");
-      console.log(currentPage);
-      fetchMoreDataContrib();
-    } */
+    setUpdateComp(true);
     setIsContributions(!isContributions);
   }
 
   useEffect(() => {
+    if (updateComp) {
+      setAllArtwork([]);
+      setCheckedProposals([]);
+      setIsAllChecked(false);
+      // setHasMoreProp(false);
+      setIsLoading(true);
+      setCurrentPage(1);
+      setUpdateComp(false);
+    }
+  }, [updateComp]);
+
+  useEffect(() => {
     if (currentPage === 1) {
-      if (isContributions) {
-        fetchMoreDataContrib();
-      } else {
-        fetchMoreData();
-      }
+      getProposals(
+        isContributions,
+        currentPage,
+        loginCtx.user?.jwt,
+        setHasMoreProp,
+        setAllArtwork,
+        setCheckedProposals,
+        setCurrentPage,
+        setIsLoading
+      );
     }
   }, [currentPage]);
 
@@ -271,30 +139,42 @@ function ValidateProp() {
             type="button"
             className="w-28 h-10 bg-red-500 text-white rounded-3xl"
             onClick={() => {
-              handleDeleteProposals();
+              handleRejectProposals();
             }}
           >
             Refuser
           </button>
         </div>
         <div className="flex justify-center mb-5">
-          <div className="form-check form-switch">
-            <input
-              className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-              type="checkbox"
-              role="switch"
-              onChange={handleSwitchChange}
-              id="flexSwitchCheckDefault"
-            />
-            <p
-              className="form-check-label inline-block text-gray-800"
-              // htmlFor="flexSwitchCheckDefault"
-            />
-          </div>
+          <p className="form-check-label inline-block text-gray-800 p-2">
+            Afficher les
+          </p>
+          {isContributions && (
+            <button
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              onClick={handleSwitchChange}
+              className="inline-block bg-slate-100 text-slate-600 font-medium text-md rounded-3xl shadow-md p-2"
+            >
+              Propositions
+            </button>
+          )}
+          {!isContributions && (
+            <button
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              onClick={handleSwitchChange}
+              className="inline-block bg-slate-100 text-slate-600 font-medium text-md rounded-3xl shadow-md p-2"
+            >
+              Contributions
+            </button>
+          )}
         </div>
         <div className="grid grid-cols-3 gap-4 content-center form-check w-full h-16 bg-slate-700 text-white rounded-3xl shadow-xl">
           <input
-            className="justify-self-center w-7 h-7 shadow-md form-check-input appearance-none border border-slate-500 rounded-sm bg-white checked:bg-slate-500 checked:border-gray-600 focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+            className="justify-self-center w-7 h-7 shadow-md form-check-input appearance-none border border-slate-500 rounded-sm bg-white checked:bg-slate-500 checked:border-gray-600 focus:outline-none transition duration-200 bg-no-repeat bg-center bg-contain float-left cursor-pointer"
             type="checkbox"
             name="allPropsCheck"
             value=""
@@ -302,24 +182,32 @@ function ValidateProp() {
             onChange={handleAllProposalsChange}
             id="flexCheckDefault"
           />
-          <p id="propositions" className="content-center col-span-2">
-            Propositions
-          </p>
+          {isContributions && (
+            <p className="content-center text-2xl font-medium col-span-2">
+              Contributions
+            </p>
+          )}
+          {!isContributions && (
+            <p className="content-center text-2xl font-medium col-span-2">
+              Propositions
+            </p>
+          )}
         </div>
       </div>
-      <div id="scrollableDiv" className="overflow-auto h-[calc(100vh-224px)]">
+      <div id="scrollableDiv" className="overflow-auto h-[calc(100vh-284px)]">
         <InfiniteScroll
           dataLength={allArtwork.length}
           next={() => {
-            if (!isContributions) {
-              console.log("fetchMoreData");
-              console.log(currentPage);
-              fetchMoreData();
-            } else {
-              console.log("fetchMoreDataContrib");
-              console.log(currentPage);
-              fetchMoreDataContrib();
-            }
+            getProposals(
+              isContributions,
+              currentPage,
+              loginCtx.user?.jwt,
+              setHasMoreProp,
+              setAllArtwork,
+              setCheckedProposals,
+              setCurrentPage,
+              setIsLoading
+            );
           }}
           hasMore={hasMoreProp}
           loader={skeletons.map((item: any) => {
@@ -347,43 +235,6 @@ function ValidateProp() {
               </div>
             );
           })}
-          endMessage={
-            <AnimatePresence initial exitBeforeEnter>
-              <motion.div
-                variants={{
-                  hidden: {
-                    scale: 0.5,
-                    y: "+30vh",
-                    opacity: 0,
-                  },
-                  visible: {
-                    y: "0",
-                    opacity: 1,
-                    scale: 1,
-                    transition: {
-                      duration: 0.5,
-                      type: "spring",
-                      damping: 25,
-                      stiffness: 400,
-                    },
-                  },
-                  exit: {
-                    x: "-30vh",
-                    opacity: 0,
-                    scale: 0.5,
-                    transition: {
-                      duration: 0.3,
-                    },
-                  },
-                }}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-              >
-                <Alert severity="success">Yay! You have seen it all</Alert>
-              </motion.div>
-            </AnimatePresence>
-          }
           scrollableTarget="scrollableDiv"
         >
           {!isLoading &&
@@ -392,14 +243,13 @@ function ValidateProp() {
             allArtwork.map((Artwork: any, index: number) => {
               return (
                 <div
-                  // eslint-disable-next-line react/no-array-index-key
                   key={Artwork.id}
                   className="grid grid-cols-6 gap-1 justify-between content-center form-check w-full h-30 text-white rounded-3xl overflow-hidden py-2"
                 >
                   <input
                     className="justify-self-center self-center shadow-md border border-slate-700 w-7 h-7 content-center form-check-input appearance-none rounded-sm bg-white checked:bg-slate-500 checked:border-gray-600 focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
                     type="checkbox"
-                    value=""
+                    value={checkedProposals[index].checked}
                     checked={checkedProposals[index].checked}
                     onChange={() => handleProposalChange(index)}
                     id="flexCheckDefault"
@@ -413,5 +263,4 @@ function ValidateProp() {
     </div>
   );
 }
-
 export default ValidateProp;
