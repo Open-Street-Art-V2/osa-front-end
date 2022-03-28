c3fd547b498d8f4b5effb0f31e2f02560c3b1a4d
kimhsn <hakimhsn06@gmail>
2022-03-28 08:53:12 +0200

    added Isloading

--- src/Pages/Admin/Contributions/UserContributions.tsx
+++ src/Pages/Admin/Contributions/UserContributions.tsx
@@ -13,43 +13,18 @@ function UserContributions() {
   const { t } = useTranslation();
   const [allArtwork, setAllArtwork] = useState<any[]>([]);
   const [checkedProposals, setCheckedProposals] = useState([] as any);
-  const [isAllChecked, setIsAllChecked] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [hasMoreProp, setHasMoreProp] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
-  const [isContributions, setIsContributions] = useState(false);
-  const [updateComp, setUpdateComp] = useState(false);
+  const [isContributions] = useState(true);
+  const [updateComp, setUpdateComp] = useState(true);
   const loginCtx = useContext(LoginContext);
   const skeletons = [1, 2, 3, 4];
-
-  const handleAllProposalsChange = () => {
-    const updatedCheckedState = checkedProposals.map((item: any) => ({
-      checked: !isAllChecked,
-      id: item.id,
-    }));
-    setCheckedProposals(updatedCheckedState);
-    setIsAllChecked(!isAllChecked);
-  };
-
-  const handleProposalChange = (position: number) => {
-    const updatedCheckedState = checkedProposals.map(
-      (item: any, index: number) =>
-        index === position ? { checked: !item.checked, id: item.id } : item
-    );
-    setCheckedProposals(updatedCheckedState);
-  };
-
-  function handleSwitchChange() {
-    setUpdateComp(true);
-    setIsContributions(!isContributions);
-  }
 
   useEffect(() => {
     if (updateComp) {
       setAllArtwork([]);
       setCheckedProposals([]);
-      setIsAllChecked(false);
-      // setHasMoreProp(false);
       setIsLoading(true);
       setCurrentPage(1);
       setUpdateComp(false);
@@ -83,53 +58,11 @@ function UserContributions() {
         </div>
         <br />
 
-        <div className="flex justify-center mb-5">
-          <p className="form-check-label inline-block text-gray-800 p-2">
-            {t("display")}
-          </p>
-          {isContributions && (
-            <button
-              type="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              onClick={handleSwitchChange}
-              className="inline-block bg-slate-100 text-slate-600 font-medium text-md rounded-3xl shadow-md p-2"
-            >
-              {t("proposals.lower")}
-            </button>
-          )}
-          {!isContributions && (
-            <button
-              type="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              onClick={handleSwitchChange}
-              className="inline-block bg-slate-100 text-slate-600 font-medium text-md rounded-3xl shadow-md p-2"
-            >
-              {t("contributions.lower")}
-            </button>
-          )}
-        </div>
         <div className="grid grid-cols-3 gap-4 content-center form-check w-full h-16 bg-slate-700 text-white rounded-3xl shadow-xl">
-          <input
-            className="justify-self-center w-7 h-7 shadow-md form-check-input appearance-none border border-slate-500 rounded-sm bg-white checked:bg-slate-500 checked:border-gray-600 focus:outline-none transition duration-200 bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-            type="checkbox"
-            name="allPropsCheck"
-            value=""
-            checked={isAllChecked}
-            onChange={handleAllProposalsChange}
-            id="flexCheckDefault"
-          />
-          {isContributions && (
-            <p className="content-center text-2xl font-medium col-span-2">
-              {t("contributions.upper")}
-            </p>
-          )}
-          {!isContributions && (
-            <p className="content-center text-2xl font-medium col-span-2">
-              {t("proposals.upper")}
-            </p>
-          )}
+          <input className="bg-slate-700" />
+          <p className="content-center text-2xl font-medium col-span-2">
+            {t("contributions.upper")}
+          </p>
         </div>
       </div>
       <div
@@ -218,27 +151,16 @@ function UserContributions() {
                     type="checkbox"
                     value={checkedProposals[index].checked}
                     checked={checkedProposals[index].checked}
-                    onChange={() => handleProposalChange(index)}
                     id="flexCheckDefault"
                   />
 
-                  {isContributions ? (
-                    <Link
-                      to="/admin/details-contribution"
-                      state={{ data: Artwork }}
-                      className="grow mx-1"
-                    >
-                      <ArtworkProposal data={Artwork} />
-                    </Link>
-                  ) : (
-                    <Link
-                      to="/admin/details-proposition"
-                      state={{ data: Artwork }}
-                      className="grow mx-1"
-                    >
-                      <ArtworkProposal data={Artwork} />
-                    </Link>
-                  )}
+                  <Link
+                    to="/admin/details-contribution"
+                    state={{ data: Artwork }}
+                    className="grow mx-1"
+                  >
+                    <ArtworkProposal data={Artwork} />
+                  </Link>
                 </div>
               );
             })}
